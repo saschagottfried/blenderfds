f13719d3d4bb5d9db67cbd86d4827b2bf9204b25
emanuele.gissi <emanuele.gissi@71380b13-3e63-4dee-54b0-4e3b1ade3fcf>
2011-03-09 08:26:04 +0000

    Replaced old content with 0.70 code

--- trunk/blenderfds/bf_ui.py
+++ trunk/blenderfds/bf_ui.py
@@ -2,7 +2,7 @@
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
+#  as published by the Free Software Foundation; either version 3
 #  of the License, or (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
@@ -18,73 +18,37 @@
 
 import bpy
 from bpy.props import *
-from blenderfds import bf_export, bf_config, bf_geometry
+from . import bf_config, bf_geometry
 from time import time
 
-# Configuration
+# Check input data and file format
 
-# UI: export menu
-
-class FdsExporter(bpy.types.Operator):
-    """Export Blender objects as FDS file."""
-    bl_idname = "export.fds"
-    bl_label = "Export as FDS file"
+def contains_quotes(string):
+    """Check if string contains quotes"""
+    return "'" in string or '"' in string
     
-    path = StringProperty(name="File Path",
-                          description="File path used for exporting the FDS file",
-                          maxlen=1024)
-                          
-    filename = StringProperty(name="File Name",
-                              description="Name of the file")
-                              
-    directory = StringProperty(name="Directory",
-                               description="Directory of the file")
-                               
-    check_existing = BoolProperty(name="Check Existing",
-                                  description="Check on overwriting existing files",
-                                  default=True,
-                                  options={'HIDDEN'})
-                                  
-    export_visible = BoolProperty(name="Visible Only",
-                                  description="Export visible objects only",
-                                  default=False)
-    
-    def execute(self, context):
-        bf_export.export_fds(context,
-                             self.properties.directory,
-                             self.properties.filename,
-                             self.properties.export_visible)
-        return {'FINISHED'}
+def contains_even_single_quotes(string):
+    """Check if string contains an even number of single quotes only"""
+    return string.count("'") % 2 == 0 and ('"' not in string)
 
-    def invoke(self, context, event):
-        wm = context.manager
-        wm.add_fileselect(self)
-        return {'RUNNING_MODAL'}
-
-# UI: edit mode tools panel
-
-class View3DPanel(bpy.types.Panel):
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'TOOLS'
-
-class VIEW3D_PT_tools_meshedit_bf(View3DPanel):
-    bl_context = "mesh_edit"
-    bl_label = "FDS Tools"
-
-    def draw(self, context):
-        layout = self.layout
-
-        col = layout.column(align=True)
-        col.operator("mesh.select_non_manifold")
+def detect_old_file_format(context, layout):
+    '''Detect and convert old file format'''
+    try:
+        tmp = context.scene['bf_type_of_header']
+        row = layout.row()
+        row.operator("wm.bf_convert_to_new_file_format", icon="ERROR")
+    except:
+        pass
+    return
 
 # UI: scene panel
 
-class SceneButtonsPanel(bpy.types.Panel):
+class SceneButtonsPanel():
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
     bl_context = "scene"
 
-class SCENE_PT_bf(SceneButtonsPanel):
+class SCENE_PT_bf(SceneButtonsPanel, bpy.types.Panel):
     '''Panel.'''
     bl_label = "FDS Case"
     
@@ -92,222 +56,247 @@ class SCENE_PT_bf(SceneButtonsPanel):
         layout = self.layout
         sc = context.scene
 
-        # Check strings
-        sc.name = bf_export.to_file_name(sc.name)
-        sc.bf_case_title = bf_export.to_no_quotes(sc.bf_case_title)
+        # detect old file
+        detect_old_file_format(context, layout)
 
-        # Case name and title
+        # Case name
         row = layout.row()
         row.prop(sc, "name", text="CHID")
+        if sc.name != bpy.path.clean_name(sc.name): 
+            row.label(icon="ERROR")
+        
+        # Case title
         row = layout.row()
-        row.prop(sc, "bf_case_title")
+        row.prop(sc, "bf_title",)
+        if contains_quotes(sc.bf_title): 
+            row.label(icon="ERROR")
         
         # Case directory
         row = layout.row()
-        row.prop(sc, "bf_case_dir")
+        row.prop(sc, "bf_directory")
 
-        # Header
+        # Configuration
         row = layout.row()
-        row.prop(sc, "bf_type_of_header", expand=True)
-        if sc.bf_type_of_header == "file":
+        row.prop(sc, "bf_config_type", expand=True)
+        if sc.bf_config_type == "FILE":
             row = layout.row()
-            row.prop(sc, "bf_header_file_path")
+            row.prop(sc, "bf_config_filepath")
 
         # Voxel size
         row = layout.row()
-        row.label(text="Voxel Size:")
-        row = layout.row()
-        row.prop(sc, "bf_voxel_size", text="")        
+        row.prop(sc, "bf_voxel_size")        
 
 # UI: object panel
 
-class ObjectButtonsPanel(bpy.types.Panel):
+class ObjectButtonsPanel():
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
     bl_context = "object"
     
-    def poll(self, context):
+    @classmethod
+    def poll(cls, context):
         ob = context.object
         return ob and ob.type == "MESH"
 
-class OBJECT_PT_bf(ObjectButtonsPanel):
+class OBJECT_PT_bf(ObjectButtonsPanel, bpy.types.Panel):
     '''Panel.'''
     bl_label = "FDS Object"
 
     def draw_header(self, context):
         layout = self.layout
         ob = context.object
-        layout.prop(ob, "bf_nl_export", text="")
-        self.bl_label = bf_config.get_nl_description(ob.bf_nl_name)
+        
+        # Export object to FDS
+        layout.prop(ob, "bf_export", text="")
+        
+        # Object namelist description to panel title
+        bf_nl = ob.bf_nl
+        self.bl_label = "FDS {} ({})".format(bf_nl, bf_config.nls.get(bf_nl, "Unknown")) 
 
     def draw(self, context):
         layout = self.layout
         ob = context.object
-        layout.enabled = ob.bf_nl_export
-        
-        # Init time
-        t = int(time())
-        
-        # Check xb
-        if ob.bf_xb == "voxels" and context.mode == "OBJECT" and not(bf_geometry.is_manifold(ob.data)):
-            ob.bf_xb = "bbox"
-            ob.bf_msg = "Object is not manifold."
-            ob.bf_msg_timer = t
-        
-        # Check one only one multiple at a time
-        if ob.bf_xb in ["voxels","faces","edges"]:
-            if ob.bf_xyz == "verts":
-                ob.bf_xyz = "none"
-                ob.bf_msg = "XB and XYZ conflicting."
-                ob.bf_msg_timer = t
-            if ob.bf_pb  == "faces":
-                ob.bf_pb = "none"
-                ob.bf_msg = "XB and PB conflicting."
-                ob.bf_msg_timer = t
-        if ob.bf_xyz == "verts":
-            if ob.bf_pb  == "faces":
-                ob.bf_pb = "none"
-                ob.bf_msg = "XYZ and PB conflicting."
-                ob.bf_msg_timer = t
-
-        # Check strings
-        ob.name = bf_export.to_no_quotes(ob.name)
-        ob.bf_nl_name = bf_export.to_no_quotes(ob.bf_nl_name)
-        ob.bf_fyi = bf_export.to_no_quotes(ob.bf_fyi)
-        ob.bf_custom_param = bf_export.to_single_quotes(ob.bf_custom_param)
+        layout.active = ob.bf_export
 
         # Init parameter choice from config and name
         nl_params = bf_config.nl_params
-        bf_nl_name = ob.bf_nl_name
-        
-        # Nl Name and FYI
-        row = layout.row()
-        row.prop(ob, "bf_nl_name")
-        row = layout.row()
-        row.prop(ob, "bf_fyi")
+        bf_nl = ob.bf_nl
 
+        # detect old file
+        detect_old_file_format(context, layout)
+        
+        # Namelist
+        row = layout.row()
+        row.prop(ob, "bf_nl")
+        if not bf_nl in bf_config.nls:
+            row.label(icon="ERROR")
+        
         # ID
         row = layout.row()
-        row.prop(ob, "bf_id", text=bf_export.get_id(ob))
+        row.active = bf_nl in nl_params["ID"]
+        row.prop(ob, "name", text="ID")
+        if contains_quotes(ob.name): 
+            row.label(icon="ERROR")
+
+        # Display as
+        row = layout.row()
+        row.prop(ob, "draw_type", text="Display as")
+                   
+        # FYI
+        row = layout.row()
+        row.prop(ob, "bf_fyi")
+        if contains_quotes(ob.bf_fyi): 
+            row.label(icon="ERROR")
 
         # SURF_ID
-        if bf_nl_name in nl_params["SURF_ID"] and ob.active_material: # must have a material, too!
+        if bf_nl in nl_params["SURF_ID"] and ob.active_material and ob.active_material.bf_export:
             row = layout.row()
-            row.prop(ob, "bf_surf_id", text=bf_export.get_surf_id(ob))
-        else: ob.bf_surf_id = False  
-
+            row.label(text="SURF_ID='" + ob.active_material.name + "'")
+            
         # SAWTOOTH
-        if bf_nl_name in nl_params["SAWTOOTH"]:
+        if bf_nl in nl_params["SAWTOOTH"]:
             row = layout.row()
-            row.prop(ob, "bf_sawtooth", text=bf_export.get_sawtooth(ob))
-        else: ob.bf_sawtooth = False
+            row.prop(ob, "bf_sawtooth", text='SAWTOOTH=.False.')
 
         # IJK
-        if bf_nl_name in nl_params["IJK"]:
+        if bf_nl in nl_params["IJK"]:
             row = layout.row()
             if ob.bf_ijk:
-                ijk = bf_export.get_ijk(ob)
-                row.prop(ob, "bf_ijk", text=ijk[3]) # use dynamic text
-                ob.bf_cell_size = ijk[2] # correct cell_size
-                row = layout.row()
-                row.label(text="Cell size:")
-                row.operator("object.bf_set_compatible_voxel")
-                row = layout.row()
-                row.prop(ob, "bf_cell_size", expand=True, text="")
+                mesh_cells = bf_geometry.get_mesh_cells(ob)
+                text = "IJK={0[0]},{0[1]},{0[2]} ({1} cells)".format(mesh_cells["ijk"],mesh_cells["quantity"])
+                row.prop(ob, "bf_ijk", text=text)
             else:
-                row.prop(ob, "bf_ijk") 
-        else: ob.bf_ijk = False
+                row.prop(ob, "bf_ijk")
+            
+            row = layout.row()
+            row.active = ob.bf_ijk
+            row.label(text="Desired Cell:")
+            row.operator("object.bf_adapt_cell_voxel")
+            row = layout.row()
+            row.active = ob.bf_ijk
+            row.prop(ob, "bf_cell_size", expand=True, text="")
         
         # Geometry
         row = layout.row()
         col1, col2, col3 = row.column(), row.column(), row.column()
         
         # XB
+        col1.active = bf_nl in nl_params["XB"]
         col1.label(text="XB:")
         col1.prop(ob, "bf_xb", text="")
-        if not(bf_nl_name in nl_params["XB"]):
-            ob.bf_xb = "none"
-            col1.enabled = False
         
         # XYZ
+        col2.active = bf_nl in nl_params["XYZ"]
         col2.label(text="XYZ:")
         col2.prop(ob, "bf_xyz", text="")
-        if not(bf_nl_name in nl_params["XYZ"]):
-            ob.bf_xyz = "none"
-            col2.enabled = False  
         
         # PB
+        col3.active = bf_nl in nl_params["PB"]
         col3.label(text="PB*:")
         col3.prop(ob, "bf_pb", text="")
-        if not(bf_nl_name in nl_params["PB"]):
-            ob.bf_pb = "none"
-            col3.enabled = False
 
-        # Error message
-        if ob.bf_msg:
-            if (t-ob.bf_msg_timer) < 2:
+        # Visualize Boxels FIXME
+        # if ob.bf_xb=="VOXELS":
+        #    row = layout.row()
+        #    row.operator("object.bf_visualize_boxels")
+
+        # Visualize Voxels FIXME
+        # if ob.bf_xb=="VOXELS":
+        #    row = layout.row()
+        #    row.operator("object.bf_visualize_voxels")
+
+        # Check XB and manifold
+        if ob.bf_xb == "VOXELS" and context.mode == "OBJECT" and (not bf_geometry.is_manifold(ob.data)):
+            row = layout.row()
+            row.label(text="Not manifold, voxels impossible", icon="ERROR")
+        
+        # Check one only one multiple at a time
+        if ob.bf_xb in ["VOXELS","FACES","EDGES"]:
+            if ob.bf_xyz == "VERTS":
                 row = layout.row()
-                row.label(icon="ERROR", text=ob.bf_msg)
-            else:
-                ob.bf_msg = ""
-            
+                row.label(text="XB and XYZ conflicting", icon="ERROR")
+
+            if ob.bf_pb  == "FACES":
+                row = layout.row()
+                row.label(text="XB and PB* conflicting", icon="ERROR")
+
+        if ob.bf_xyz == "VERTS":
+            if ob.bf_pb  == "FACES":
+                row = layout.row()
+                row.label(text="XYZ and PB* conflicting", icon="ERROR")
+
         # Custom param
         col = layout.column()
-        col.label(text="Custom Parameters:")
+        col.label(text="Custom " + bf_nl + " Parameters:")
         col.prop(ob, "bf_custom_param", text="")
-
+        if not contains_even_single_quotes(ob.bf_custom_param): 
+            col.label(text="Use Matched Single Quotes", icon="ERROR")
+        
 # UI: material panel
 
-class MaterialButtonsPanel(bpy.types.Panel):
+class MaterialButtonsPanel():
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
     bl_context = "material"
-    
-    def poll(self, context):
-        ma = context.material
-        return ma
 
-class MATERIAL_PT_bf(MaterialButtonsPanel):
+    @classmethod    
+    def poll(cls, context):
+        ma = context.material
+        ob = context.object
+        return ma and ob and ob.type == "MESH" and ob.bf_nl in bf_config.nl_params["SURF_ID"] and ob.bf_export
+
+class MATERIAL_PT_bf(MaterialButtonsPanel, bpy.types.Panel):
     '''Panel.'''
     bl_label = "FDS SURF (Boundary Condition)"
+
+    def draw_header(self, context):
+        layout = self.layout
+        ma = context.material
+        
+        # Export material to FDS
+        layout.prop(ma, "bf_export", text="")
     
     def draw(self, context):
         layout = self.layout
         ma = context.material
+        ob = context.object
+        layout.active = ma.bf_export
 
-        # Check strings
-        ma.name = bf_export.to_no_quotes(ma.name)
-        ma.bf_fyi = bf_export.to_no_quotes(ma.bf_fyi)
-        ma.bf_custom_param = bf_export.to_single_quotes(ma.bf_custom_param)
+        # detect old file
+        detect_old_file_format(context, layout)
 
+        # Check predefined SURFs. If not propose operator
+        mas_predefined = bf_config.mas_predefined
+        mas = bpy.data.materials.keys()
+        if set(mas_predefined) & set(mas) != set(mas_predefined):
+            row = layout.row()
+            row.operator("material.bf_create_predefined", icon="ERROR")
+
+        # ID
+        row = layout.row()
+        row.template_ID(ob, "active_material", new="material.new")
+        if contains_quotes(ob.active_material.name):
+            row.label(icon="ERROR")
+        
         # FYI
         row = layout.row()
         row.prop(ma, "bf_fyi")
-        
+        if contains_quotes(ma.bf_fyi):
+            row.label(icon="ERROR")
+
         # RGB
         row = layout.row()
-        row.prop(ma, "bf_rgb")
-        row.prop(ma, "diffuse_color", text="")
+        row.prop(ma, "diffuse_color", text="RGB")
         
         # TRANSPARENCY
         row = layout.row()
-        row.prop(ma, "bf_transparency")
-        row.prop(ma, "alpha", text="")
+        row.prop(ma, "use_transparency", text="TRANSPARENCY")
+        row.prop(ma, "alpha")
         
-        # Custom param and FYI
+        # Custom param
         col = layout.column()
-        col.label(text="Custom Parameters:")
+        col.label(text="Custom SURF Parameters:")
         col.prop(ma, "bf_custom_param", text="")
-
-# UI: operators
-        
-class OBJECT_OT_bf_set_compatible_voxel(bpy.types.Operator):
-    bl_label = "Adapt Voxel"
-    bl_idname = "object.bf_set_compatible_voxel"
-    bl_description = "Set compatible voxel size in scene"
-
-    def invoke(self, context, event):
-        cell_size = bf_export.get_ijk(context.object)[2]
-        context.scene.bf_voxel_size = (cell_size[0]/2, cell_size[1]/2, cell_size[2]/2)
-        return{'FINISHED'}
-        
+        if not contains_even_single_quotes(ma.bf_custom_param): 
+            col.label(text="Use Matched Single Quotes.", icon="ERROR")
+            
