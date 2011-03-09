87c3c67f226f2c9fd1f4b5d2b58b452362936dcf
emanuele.gissi <emanuele.gissi@71380b13-3e63-4dee-54b0-4e3b1ade3fcf>
2011-03-09 08:25:20 +0000

    Replaced old content with 0.70 code

--- trunk/blenderfds/bf_geometry.py
+++ trunk/blenderfds/bf_geometry.py
@@ -2,7 +2,7 @@
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
+#  as published by the Free Software Foundation; either version 3
 #  of the License, or (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
@@ -17,8 +17,10 @@
 # ##### END GPL LICENSE BLOCK #####
 
 import bpy
-from Mathutils import Vector
-from Geometry  import TriangleNormal
+from mathutils import Vector, geometry
+from time import time
+
+# FIXME psyco
 
 ### Working on Blender objects and meshes
 # input: ob or me
@@ -27,14 +29,9 @@ from Geometry  import TriangleNormal
 def get_global_mesh(ob):
     """Return object mesh modified and transformed in global coordinates."""
     # Get ob mesh in global coordinates and return it
-    me = ob.create_mesh(True, "PREVIEW") # apply modifiers to mesh
-    me.transform(ob.matrix) # transform mesh in global coordinates
+    me = ob.create_mesh(bpy.context.scene, True, "PREVIEW") # apply modifiers
+    me.transform(ob.matrix_world) # transform mesh in global coordinates
     return me
-    
-def is_mesh(ob):
-    """Check if object ob is of MESH type"""
-    if ob.type != "MESH": return False
-    return True
 
 def is_manifold(me):
     """Check if mesh me is manifold"""
@@ -55,8 +52,8 @@ def is_manifold(me):
 
 class _Fa3:
     """Used to triangulate the mesh and simplify the notation"""
-    def __init__(self, verts, normal):
-        self.verts = verts
+    def __init__(self, vertices, normal):
+        self.vertices = vertices
         self.normal = normal
 
 def _is_inside_z_projection(point, fa3s):
@@ -64,9 +61,9 @@ def _is_inside_z_projection(point, fa3s):
     # Calculate boundary edges of the z projection surface of fa3s
     boundary_edges = set()
     for fa3 in fa3s:
-        for edge in ((fa3.verts[0], fa3.verts[1]),
-                     (fa3.verts[1], fa3.verts[2]),
-                     (fa3.verts[2], fa3.verts[0])):
+        for edge in ((fa3.vertices[0], fa3.vertices[1]),
+                     (fa3.vertices[1], fa3.vertices[2]),
+                     (fa3.vertices[2], fa3.vertices[0])):
             if (edge[0], edge[1]) in boundary_edges:
                 boundary_edges.remove((edge[0], edge[1]))
                 continue
@@ -79,10 +76,10 @@ def _is_inside_z_projection(point, fa3s):
     for edge in boundary_edges:
         p0, p1 = edge[0].co, edge[1].co
         if (p0[1] <= point[1] < p1[1]):
-            if TriangleNormal(point, p0, p1)[2] < 0.0: inside = not inside
+            if geometry.normal(point, p0, p1)[2] < 0.0: inside = not inside
             continue
         if (p1[1] <= point[1] < p0[1]):
-            if TriangleNormal(point, p0, p1)[2] > 0.0: inside = not inside
+            if geometry.normal(point, p0, p1)[2] > 0.0: inside = not inside
             continue
     return inside
 
@@ -92,15 +89,15 @@ def _triangulate(me):
     # me.faces -> fa3s, face -> fa3
     fa3s = list()
     for face in me.faces:
-        fa3s.append(_Fa3(verts=[me.verts[face.verts[0]],
-                                me.verts[face.verts[1]],
-                                me.verts[face.verts[2]]],
-                                normal=face.normal))
-        if len(face.verts) != 3: # If it's a quad, add another face
-            fa3s.append(_Fa3(verts=[me.verts[face.verts[0]],
-                                    me.verts[face.verts[2]],
-                                    me.verts[face.verts[3]]],
-                                    normal=face.normal))
+        fa3s.append(_Fa3(vertices=[me.vertices[face.vertices[0]],
+                                   me.vertices[face.vertices[1]],
+                                   me.vertices[face.vertices[2]]],
+                         normal=face.normal))
+        if len(face.vertices) != 3: # If it's a quad, add another face
+            fa3s.append(_Fa3(vertices=[me.vertices[face.vertices[0]],
+                                       me.vertices[face.vertices[2]],
+                                       me.vertices[face.vertices[3]]],
+                             normal=face.normal))
     return fa3s
 
 def _voxelize(ob, voxel_size, solid):
@@ -108,34 +105,33 @@ def _voxelize(ob, voxel_size, solid):
 This code is copy-pasted from Cells v1.2_248 (GPL) for Blender
 by Michael Schardt (m.schardt@web.de)
 """
-    # Check voxel_size
-    if voxel_size[0] <= 0. or voxel_size[1] <= 0. or voxel_size[2] <= 0.:
-        raise ValueError("Cubic voxel size must be a positive float number!")
+    # Timing
+    t0 = time()
     # Get ob mesh in global coordinates and triangulate it
     me = get_global_mesh(ob)
     fa3s = _triangulate(me)
-    # Get bin vertices: voxels_verts
+    # Get bin vertices: voxels_vertices
     # it uses vertices from Blender mesh me
-    voxels_verts = dict() # {vert obj: (3,7,3), vert obj: (1,2,3), ...}
-    for vert in me.verts:
-        coords = vert.co
-        voxels_verts[vert] = (int(round(coords[0]/voxel_size[0])),
-                              int(round(coords[1]/voxel_size[1])),
-                              int(round(coords[2]/voxel_size[2])))
+    voxels_vertices = dict() # {vertex obj: (3,7,3), vertex obj: (1,2,3), ...}
+    for vertex in me.vertices:
+        coords = vertex.co
+        voxels_vertices[vertex] = (int(round(coords[0]/voxel_size[0])),
+                                   int(round(coords[1]/voxel_size[1])),
+                                   int(round(coords[2]/voxel_size[2])))
     # Get bin faces: voxels
     voxels = dict()
     for fa3 in fa3s:
-        verts = fa3.verts
+        vertices = fa3.vertices
         # Calc min and max of fa3 integer delta x
-        fidxs = [voxels_verts[vert][0] for vert in verts]
+        fidxs = [voxels_vertices[vertex][0] for vertex in vertices]
         fidxs.sort()
         min_fidx, max_fidx = fidxs[0], fidxs[-1]
         # Calc min and max of fa3 integer delta y
-        fidys = [voxels_verts[vert][1] for vert in verts]
+        fidys = [voxels_vertices[vertex][1] for vertex in vertices]
         fidys.sort()
         min_fidy, max_fidy = fidys[0], fidys[-1]
         # Calc min and max of fa3 integer delta z
-        fidzs = [voxels_verts[vert][2] for vert in verts]
+        fidzs = [voxels_vertices[vertex][2] for vertex in vertices]
         fidzs.sort()
         min_fidz, max_fidz = fidzs[0], fidzs[-1]
         # Fast path: used especially for small fa3s spanning a single voxel only
@@ -164,8 +160,8 @@ by Michael Schardt (m.schardt@web.de)
         # Long path: fa3 spans multiple voxels in more than one direction
         a0 = fa3.normal
         r0 = 0.5 * (abs(a0[0])*voxel_size[0] + abs(a0[1])*voxel_size[1] + abs(a0[2])*voxel_size[2])
-        es = [Vector(1.0, 0.0, 0.0), Vector(0.0, 1.0, 0.0), Vector(0.0, 0.0, 1.0)]
-        cc = Vector(0.0, 0.0, 0.0)
+        es = [Vector((1.0, 0.0, 0.0)), Vector((0.0, 1.0, 0.0)), Vector((0.0, 0.0, 1.0))]
+        cc = Vector((0.0, 0.0, 0.0))
         for fidx in range(min_fidx, max_fidx + 1):
             cc[0] = fidx * voxel_size[0]
             for fidy in range(min_fidy, max_fidy + 1):
@@ -174,13 +170,13 @@ by Michael Schardt (m.schardt@web.de)
                     cc[2] = fidz * voxel_size[2]
                     if not solid and (fidx, fidy, fidz) in voxels: continue
                        # voxel already populated -> no further processing needed for hollow model
-                    verts_wrk = [vert.co - cc for vert in verts]
-                    if not (-r0 <= a0 * verts_wrk[0] <= r0): continue
+                    vertices_wrk = [vertex.co - cc for vertex in vertices]
+                    if not (-r0 <= a0 * vertices_wrk[0] <= r0): continue
                        # voxel not intersecting fa3 hyperplane
                     # Check overlap of voxel with fa3 (separating axis theorem)                     
-                    fa3s_wrk = [verts_wrk[1] - verts_wrk[0],
-                                verts_wrk[2] - verts_wrk[1],
-                                verts_wrk[0] - verts_wrk[2]]
+                    fa3s_wrk = [vertices_wrk[1] - vertices_wrk[0],
+                                vertices_wrk[2] - vertices_wrk[1],
+                                vertices_wrk[0] - vertices_wrk[2]]
                     overlap = True
                     for fa3_wrk in fa3s_wrk:
                         if not overlap: break
@@ -188,7 +184,7 @@ by Michael Schardt (m.schardt@web.de)
                             if not overlap: break
                             a = e.cross(fa3_wrk)
                             r = 0.5 * (abs(a[0])*voxel_size[0] + abs(a[1])*voxel_size[1] + abs(a[2])*voxel_size[2] )
-                            ds = [a * vert_wrk for vert_wrk in verts_wrk]
+                            ds = [a * vertex_wrk for vertex_wrk in vertices_wrk]
                             ds.sort()
                             if (ds[0] > r or ds[-1] < -r): overlap = False              
                     if overlap: voxels.setdefault((fidx, fidy, fidz), set()).add(fa3)
@@ -207,7 +203,7 @@ by Michael Schardt (m.schardt@web.de)
         idzs.sort() # voxel[0] is its fidz from the key
         min_idz, max_idz = idzs[0], idzs[-1]
         # init point_test and loop
-        point_test = Vector(0.0, 0.0, 0.0)
+        point_test = Vector((0.0, 0.0, 0.0))
         for idx in range(min_idx, max_idx + 1):
             point_test[0] = idx * voxel_size[0]
             for idy in range(min_idy, max_idy + 1):
@@ -228,7 +224,7 @@ by Michael Schardt (m.schardt@web.de)
                             if fa3_normal_z <= 0.0: fa3s_neg.append(fa3_wrk)                        
                             fa3s_tested.add(fa3_wrk)
                         # check if point_test inside z projections       
-                        if fa3s_pos:                                                                      
+                        if fa3s_pos:
                             if _is_inside_z_projection(point_test, fa3s_pos):
                                 odd_parity = not odd_parity
                         if fa3s_neg:
@@ -242,79 +238,23 @@ by Michael Schardt (m.schardt@web.de)
     # voxels is exported as a set
     voxels = set([(voxel[0], voxel[1], voxel[2]) for voxel in voxels]) # no more linked to me
     bpy.data.meshes.remove(me)
+    print("   voxelize:", time()-t0)
     return voxels
-
-def _test_voxelize(context, voxel_size):
-    """Test _voxelize function"""
-    # Import Psyco if available
-    try:
-        import psyco
-        psyco.full()
-    except ImportError:
-        pass
-    # Then start
-    for ob in [obs for obs in context.scene.objects if obs.is_visible()]: #visible?
-    #for ob in bpy.context.selected_objects:
-        # Checks
-        if not is_mesh(ob): continue
-        if not is_manifold(ob.data): continue
-        # Voxelization
-        print("Voxelizing:", ob.name)
-        solid = True
-        voxels = _voxelize(context, ob, solid)
-        # Create a new mesh with result and add vertices in flat coords
-        me_new = bpy.data.meshes.new(ob.name+"_voxelized")
-        me_new.add_geometry(len(voxels), 0, 0)
-        flat_coords = list()
-        for voxel in voxels:
-            flat_coords.append(voxel[0] * voxel_size[0])
-            flat_coords.append(voxel[1] * voxel_size[1])
-            flat_coords.append(voxel[2] * voxel_size[2])
-        me_new.verts.foreach_set("co", flat_coords)
-        # Create a new object, link the new mesh, link the object to the scene
-        ob_new = bpy.data.objects.new(ob.name+"_voxelized", me_new)
-        ob_new.layers = ob.layers
-        scene = bpy.context.scene
-        scene.objects.link(ob_new)
-        scene.objects.active = ob_new
-        ob_new.selected = True
-    print("Done")
-    
-def _test_get_boxels(context, voxel_size):
-    """Test get_boxels function"""
-    # Import Psyco if available
-    try:
-        import psyco
-        psyco.full()
-    except ImportError:
-        pass
-    # Then start
-    for ob in [obs for obs in context.scene.objects if obs.is_visible()]: #visible?
-    #for ob in bpy.context.selected_objects:
-        timing("Treating {}".format(ob.name,))
-        if not is_mesh(ob): continue
-        if not is_manifold(ob.data): continue
-        solid = True
-        timing("Start")
-        boxes = get_boxels(context, ob, solid)
-        timing("boxelize")
-        print("boxes", boxes)
-    print("Done")
 
 ### Extracting coordinates from objects
 # input:  ob
 # output: [(x0,x1,y0,y1,z0,z1, ), ] or
 #         [(x0,y0,z0, ), ]
 
-def get_bb(ob):
+def get_bbox(ob):
     """Return object bounding box in global coordinates"""
     # Init mesh
     me = get_global_mesh(ob)
     # Calc the bounding box in global coordinates
-    bbminx, bbminy, bbminz = me.verts[0].co
-    bbmaxx, bbmaxy, bbmaxz = me.verts[0].co
-    for vert in me.verts:
-        x, y, z = vert.co
+    bbminx, bbminy, bbminz = me.vertices[0].co
+    bbmaxx, bbmaxy, bbmaxz = me.vertices[0].co
+    for vertex in me.vertices:
+        x, y, z = vertex.co
         bbminx, bbminy, bbminz = min(bbminx, x), min(bbminy, y), min(bbminz, z)
         bbmaxx, bbmaxy, bbmaxz = max(bbmaxx, x), max(bbmaxy, y), max(bbmaxz, z)
     return [(bbminx, bbmaxx, bbminy, bbmaxy, bbminz, bbmaxz,), ]
@@ -324,12 +264,12 @@ def get_faces(ob):
     result = []
     me = get_global_mesh(ob)
     for face in me.faces:
-        verts = [me.verts[vert] for vert in face.verts]
+        vertices = [me.vertices[vertex] for vertex in face.vertices]
         # Calc the bounding box in global coordinates
-        bbminx, bbminy, bbminz = verts[0].co
-        bbmaxx, bbmaxy, bbmaxz = verts[0].co
-        for vert in verts[1:]:
-            x, y, z = vert.co
+        bbminx, bbminy, bbminz = vertices[0].co
+        bbmaxx, bbmaxy, bbmaxz = vertices[0].co
+        for vertex in vertices[1:]:
+            x, y, z = vertex.co
             bbminx, bbminy, bbminz = min(bbminx, x), min(bbminy, y), min(bbminz, z)
             bbmaxx, bbmaxy, bbmaxz = max(bbmaxx, x), max(bbmaxy, y), max(bbmaxz, z)
         bbd = [(bbmaxx - bbminx, 2), (bbmaxy - bbminy, 1), (bbmaxz - bbminz, 0)]
@@ -341,23 +281,33 @@ def get_faces(ob):
     result.sort()
     return result
 
+def get_planes(ob):
+    """Return object planes with orientation and coordinate"""
+    xbs = get_faces(ob)
+    result = tuple()
+    for xb in xbs:
+        if   xb[1] - xb[0] < .000001: result += (("X", xb[0]),)
+        elif xb[3] - xb[2] < .000001: result += (("Y", xb[2]),)
+        elif xb[5] - xb[4] < .000001: result += (("Z", xb[4]),)
+    return result
+
 def get_edges(ob):
     """Return object edges in global coordinates"""
     result = []
     me = get_global_mesh(ob)
     for edge in me.edges:
-        pt0x, pt0y, pt0z = me.verts[edge.verts[0]].co
-        pt1x, pt1y, pt1z = me.verts[edge.verts[1]].co
+        pt0x, pt0y, pt0z = me.vertices[edge.vertices[0]].co
+        pt1x, pt1y, pt1z = me.vertices[edge.vertices[1]].co
         result.append((pt0x, pt1x, pt0y, pt1y, pt0z, pt1z,),)
     result.sort()
     return result
 
-def get_verts(ob):
+def get_vertices(ob):
     """Return object vertices in global coordinates"""
     result = []
     me = get_global_mesh(ob)
-    for vert in me.verts:
-        pt0x, pt0y, pt0z = vert.co
+    for vertex in me.vertices:
+        pt0x, pt0y, pt0z = vertex.co
         result.append((pt0x, pt0y, pt0z),)
     result.sort()
     return result
@@ -370,6 +320,8 @@ def get_boxels(ob, voxel_size, solid):
     """Return object boxels by summing voxels in as big as possible boxes"""
     # Get voxels
     voxels = _voxelize(ob, voxel_size, solid)
+    # Timing
+    t0 = time()
     # Init boxes and start. boxes is now in integer voxel coordinates
     boxes = list()
     while voxels:
@@ -432,6 +384,7 @@ def get_boxels(ob, voxel_size, solid):
         boxes.append(box)
     # Sort boxes and export boxes in global coos
     boxes.sort()
+    print("   boxelize:", time()-t0)
     return [((box[0]-.5) * voxel_size[0], (box[1]+.5) * voxel_size[0],
              (box[2]-.5) * voxel_size[1], (box[3]+.5) * voxel_size[1],
              (box[4]-.5) * voxel_size[2], (box[5]+.5) * voxel_size[2]) for box in boxes]
@@ -449,8 +402,8 @@ for publishing this code"""
     limit = n**0.5  
     while i <= limit:  
         if n % i == 0:  
-            yield i  
-            n = n / i  
+            yield i
+            n = n / i
             limit = n**0.5  
         else:
             i += 1  
@@ -468,31 +421,11 @@ def _n_for_poisson(n):
 def get_mesh_cells(ob):
     """Calc optimized and valid value for MESH IJK from voxel size"""
     cell_size = ob.bf_cell_size
-    bbminx, bbmaxx, bbminy, bbmaxy, bbminz, bbmaxz = get_bb(ob)[0]
+    bbminx, bbmaxx, bbminy, bbmaxy, bbminz, bbmaxz = get_bbox(ob)[0]
     ijk = (int(round((bbmaxx - bbminx) / cell_size[0])),
            _n_for_poisson(int(round((bbmaxy - bbminy) / cell_size[1]))),
            _n_for_poisson(int(round((bbmaxz - bbminz) / cell_size[2]))) )
     quantity = ijk[0] * ijk[1] * ijk[2] 
     cell_size = ((bbmaxx - bbminx) / ijk[0], (bbmaxy - bbminy) / ijk[1], (bbmaxz - bbminz) / ijk[2] )
-    return [ijk, quantity, cell_size]
-
-### Main for tests
-#
-#
-
-t0 = 0 # global timer
-def timing(what):
-    global t0
-    import time
-    t1 = time.time()
-    print(what, "at", round(t1-t0, 3))
-    t0 = t1
-
-if __name__ == "__main__":
-    import time
-    voxel_size = (.2,.2,.2)
-    time0 = time.time()
-    _test_get_boxels(bpy.context, voxel_size)
-    time1 = time.time()
-    print("Total time:", time1-time0)
+    return {"ijk": ijk, "quantity": quantity, "cell_size": cell_size}
 
