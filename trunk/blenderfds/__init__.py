bbb0a40df6613534c59cd2df916bd98653bb1c3d
emanuele.gissi <emanuele.gissi@71380b13-3e63-4dee-54b0-4e3b1ade3fcf>
2011-03-09 08:22:11 +0000

    Replaced old code with version 0.70

--- trunk/blenderfds/__init__.py
+++ trunk/blenderfds/__init__.py
@@ -2,7 +2,7 @@
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
+#  as published by the Free Software Foundation; either version 3
 #  of the License, or (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
@@ -15,199 +15,51 @@
 #  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # ##### END GPL LICENSE BLOCK #####
-"""BlenderFDS, an open tool for the NIST Fire Dynamics Simulator."""
-    
-bl_addon_info = {
-    "name": "BlenderFDS",
-    "author": "Emanuele Gissi",
-    "blender": (2, 5, 2),
-    "category": "Export",
-    "location": "File > Export > FDS Case (.fds)",
-    "description": "BlenderFDS, an open tool for the NIST Fire Dynamics Simulator",
-    "url": "http://code.google.com/p/blender-fds/",
-    "category": "Export",
-}
+"BlenderFDS, an open tool for the NIST Fire Dynamics Simulator."
 
-# FIXME Check scene name
+# Max number of colums for output file
+col_max = 100
 
-import bpy
+# Namelist names
+nls = {"OBST": "Obstruction",
+       "HOLE": "Obstruction Cutout",
+       "VENT": "Boundary Condition Patch",
+       "DEVC": "Device",
+       "SLCF": "Slice File",
+       "PROF": "Wall Profile Output",
+       "MESH": "Domain of Simulation",
+       "INIT": "Initial Condition",
+       "ZONE": "Pressure Zone",
+       "EVAC": "Evac Agents Position",
+       "EVHO": "Evac Agents Hole",
+       "EXIT": "Evac Exit",
+       "ENTR": "Evac Entry",
+       "DOOR": "Evac Door",
+       "CORR": "Evac Corridor",
+       "EVSS": "Evac Incline",
+       "STRS": "Evac Staircase",
+      }
 
-def menu_export(self, context):
-    from blenderfds import bf_ui
-    sc = context.scene
-    default_path = bpy.data.filename.replace(".blend", ".fds")
-    if sc.name: default_path = bpy.app.home + "/" + sc.name + ".fds"
-    if sc.bf_case_dir: default_path = sc.bf_case_dir + default_path
-    self.layout.operator(bf_ui.FdsExporter.bl_idname, text="FDS Case (.fds)").path = default_path
+# Namelist parameters FIXME ID, "SLCF",
+nl_params = {"ID":       ("DEVC", "PROF", "HOLE", "INIT", "MESH", "OBST", "VENT", "ZONE", "EVAC", "EVHO", "EXIT", "ENTR", "DOOR", "CORR", "EVSS", "STRS"), 
+             "SURF_ID":  ("DEVC", "OBST","VENT"),
+             "SAWTOOTH": ("OBST", ),
+             "IJK":      ("MESH", ),
+             "XB":       ("DEVC", "HOLE", "INIT", "MESH", "OBST", "SLCF", "VENT", "ZONE", "EVAC", "EVHO", "EXIT", "ENTR", "DOOR", "CORR", "EVSS", "STRS"),
+             "XYZ":      ("DEVC", "PROF", "VENT", "EXIT", "DOOR", "STRS"),
+             "PB":       ("SLCF", "VENT"),
+             }
 
-def register():
-    from blenderfds import bf_ui
-    
-    # scene properties
+# Namelist groups, a tuple for sorting
+# The last group is used to collect all remaining namelist groups.
+nl_groups = (("Computational domain",     ("MESH", "INIT", "ZONE")),
+             ("Geometry",                 ("OBST", "HOLE", "VENT")),
+             ("Evacuation",               ("EVAC","EVHO","EXIT","ENTR","DOOR","CORR","EVSS","STRS")),
+             ("Control logic and output", ("DEVC", "SLCF", "PROF")),
+             ("Others",                   ()),
+             )
 
-    bpy.types.Scene.StringProperty(attr="bf_case_dir",
-                                   name="Case Dir",
-                                   subtype="FILE_PATH",
-                                   description="FDS case directory") # FIXME DIRECTORY_PATH
-   
-    bpy.types.Scene.StringProperty(attr="bf_case_title",
-                                   name="TITLE",
-                                   maxlen=32,
-                                   description="Case title")
-    
-    bpy.types.Scene.FloatVectorProperty(attr="bf_voxel_size",
-                                        name="Voxel Size",
-                                        description="Voxel size",
-                                        step=1, precision=3,
-                                        subtype="XYZ",
-                                        default=(.20, .20, .20), min=.001, max=10.)
-
-    items_list = [("auto", "Auto Header", "Automatic header"),
-                  ("file", "File", "Custom header file",)]
-    bpy.types.Scene.EnumProperty(attr="bf_type_of_header",
-                                 name="Type of Header",
-                                 items=items_list,
-                                 description="Type of header",
-                                 default="automatic")
-                                 
-    bpy.types.Scene.StringProperty(attr="bf_header_file_path",
-                                   name="Header File",
-                                   subtype="FILE_PATH",
-                                   description="Header file",
-                                   default="header.fds")
-                                   
-    # object properties
-    
-    bpy.types.Object.BoolProperty(attr="bf_nl_export",
-                                  name="Export",
-                                  description="Export object to FDS",
-                                  default=False)
-   
-    bpy.types.Object.StringProperty(attr="bf_nl_name",
-                                    name="& Name",
-                                    description="Namelist name",
-                                    default="OBST")
-
-    bpy.types.Object.BoolProperty(attr="bf_id",
-                                  name="ID",
-                                  description="Set ID parameter",
-                                  default=True)
-                                  
-    items_list = [("none",   "None",   "None"),
-                  ("voxels", "Voxels", "Voxelized solid"),
-                  ("bbox",   "BBox",   "Bounding box"),
-                  ("faces",  "Faces",  "Faces, one for each face of this object"),
-                  ("edges",  "Edges",  "Segments, one for each edge of this object")]
-    bpy.types.Object.EnumProperty(attr="bf_xb",
-                                  name="XB",
-                                  items=items_list,
-                                  description="Set XB parameter",
-                                  default="none")
-    
-    items_list = [("none",   "None",   "None"),
-                  ("center", "Center", "Point, corresponding to center point of this object"),
-                  ("verts",  "Verts",  "Points, one for each vertex of this object"),]
-    bpy.types.Object.EnumProperty(attr="bf_xyz",
-                                  name="XYZ",
-                                  items=items_list,
-                                  description="Set XYZ parameter",
-                                  default="none")
-    
-    items_list = [("none",   "None",   "None"),
-                  ("faces",  "Faces",  "Planes, one for each face of this object"),]
-    bpy.types.Object.EnumProperty(attr="bf_pb",
-                                  name="PB*",
-                                  items=items_list,
-                                  description="Set PBX, PBY, PBZ parameter",
-                                  default="none")
-
-    bpy.types.Object.FloatVectorProperty(attr="bf_cell_size",
-                                         name="Cell Size",
-                                         description="Cell size",
-                                         step=1, precision=3,
-                                         subtype="XYZ",
-                                         default=(.20, .20, .20), min=.001, max=10.)
-    
-    bpy.types.Object.BoolProperty(attr="bf_surf_id",
-                                  name="SURF_ID",
-                                  description="Set SURF_ID parameter to corresponding Blender material",
-                                  default=False)
-    
-    bpy.types.Object.BoolProperty(attr="bf_sawtooth",
-                                  name="SAWTOOTH",
-                                  description="Set SAWTOOTH=.FALSE. parameter",
-                                  default=False)
-    
-    bpy.types.Object.BoolProperty(attr="bf_ijk",
-                                  name="IJK",
-                                  description="Set IJK parameter optimized for voxel size",
-                                  default=False)
-    
-    bpy.types.Object.StringProperty(attr="bf_fyi",
-                                    name="FIY",
-                                    maxlen=32,
-                                    description="Set FIY parameter")
-    
-    bpy.types.Object.StringProperty(attr="bf_custom_param",
-                                    name="Custom Parameters",
-                                    description="Set custom parameters (exported verbatim)")
-    
-    bpy.types.Object.StringProperty(attr="bf_msg", name="Msg") # error message
-    bpy.types.Object.IntProperty(attr="bf_msg_timer", name="Timer") # timer for error message
-        
-    # material properties
-    
-    bpy.types.Material.BoolProperty(attr="bf_rgb",
-                                    name="RGB",
-                                    description="Set RGB parameter to corresponding Blender material diffuse color",
-                                    default=True)
-
-    bpy.types.Material.BoolProperty(attr="bf_transparency",
-                                    name="TRANSPARENCY",
-                                    description="Set TRANSPARENCY parameter to corresponding Blender material alpha",
-                                    default=False)
-    
-    bpy.types.Material.StringProperty(attr="bf_fyi",
-                                      name="FIY",
-                                      maxlen=32,
-                                      description="Set FIY parameter")
-                                       
-    bpy.types.Material.StringProperty(attr="bf_custom_param",
-                                      name="Custom Parameters",
-                                      description="Set custom parameters (exported verbatim)")
-     
-    # panels
-    bpy.types.register(bf_ui.SCENE_PT_bf)
-    bpy.types.register(bf_ui.OBJECT_PT_bf)
-    bpy.types.register(bf_ui.MATERIAL_PT_bf)
-    
-    # export menu
-    bpy.types.register(bf_ui.FdsExporter)
-    bpy.types.INFO_MT_file_export.prepend(menu_export)
-    
-    # tools
-    bpy.types.register(bf_ui.VIEW3D_PT_tools_meshedit_bf)
-    
-    # ops
-    bpy.types.register(bf_ui.OBJECT_OT_bf_set_compatible_voxel)
-
-def unregister():
-    from blenderfds import bf_ui
-    
-    # panels
-    bpy.types.unregister(bf_ui.SCENE_PT_bf)
-    bpy.types.unregister(bf_ui.OBJECT_PT_bf)
-    bpy.types.unregister(bf_ui.MATERIAL_PT_bf)
-
-    # export menu
-    bpy.types.unregister(bf_ui.FdsExporter)
-    bpy.types.INFO_MT_file_export.remove(menu_export)
-
-    # tools
-    bpy.types.unregister(bf_ui.VIEW3D_PT_tools_meshedit_bf)
-
-    # ops
-    bpy.types.unregister(bf_ui.OBJECT_OT_bf_set_compatible_voxel)
-    
-if __name__ == "__main__":
-    register()
+# Predefined FDS SURF boundary conditions (Blender materials)
+mas_predefined = ("OPEN", "MIRROR", "INERT")
+# They are created by the hardcoded operator:
+# class MATERIAL_OT_bf_create_predefined(bpy.types.Operator):
